7556d5853f3bddd7e0b6ca5e2b720ae73688143a
Leifer Mendez <leifer33@gmail.com>
2021-04-27 10:22:03 +0200

    Change to SMTP Gmail

--- mail.js
+++ mail.js
@@ -13,6 +13,7 @@ const sendMail = async (dataMail) => {
     transporter = nodemailer.createTransport({
         host: process.env.MAIL_SMTP,
         port: process.env.MAIL_PORT,
+        secure: true,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
